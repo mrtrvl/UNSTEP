4e94c07e0e02a6509417050807e6db48d6a207e2
Kristjan Toots <kristjan.toots@gmail.com>
2017-08-31 19:52:15 +0300

    Create UNSTEP.Common library

---
+++ src/UNSTEP.Common/ThrowIf.cs
@@ -0,0 +1,19 @@
+﻿namespace UNSTEP.Common
+{
+    using System;
+
+    public static class ThrowIf
+    {
+        public static void IsNull(object obj, string parameterName)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(parameterName);
+        }
+
+        public static void IsNullOrEmpty(string value, string parameterName)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(parameterName);
+        }
+    }
+}
