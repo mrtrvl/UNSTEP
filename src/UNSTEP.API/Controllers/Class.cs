dddc7a2586053e04c502284ce8f2aa200b381f52
Unknown <mrt@mrt.ee>
2017-09-17 21:42:42 +0300

    Add Health controller

---
+++ src/UNSTEP.API/Controllers/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UNSTEP.API.Controllers
+{
+    public class HealthController : Controller
+    {
+        [HttpGet("api/health")]
+        public IActionResult GetHealth()
+        {
+            return this.Ok();
+        }
+    }
+}
